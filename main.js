b3fd8b9c94f37b79cf831589ddf0c9963d969484
Van der Meulen Falko <falko.vandermeulen@student.hogent.be>
2020-09-12 00:49:50 +0200

    gilde update

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ client.once('ready', () => {
 );
 
 client.on('message', message => {
+
     if(message.content.toLowerCase().includes(' han' ) || message.content.toLowerCase() === 'han' || message.content.toLowerCase().includes('jisung'))
-    message.react('752655278985707521');
+        message.react('752655278985707521');
+
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -38,38 +40,51 @@ client.on('message', message => {
         ];
         let randomgetal = Math.floor(Math.random()*songs.length);
         
-            message.channel.send(songs[randomgetal]);
+        message.channel.send(songs[randomgetal]);
             
     } else if(command === 'wave'){
+
         const gifs = ['https://cdn.discordapp.com/attachments/702535108229333004/752663021276364901/hannie.gif',
-            'https://cdn.discordapp.com/attachments/702535108229333004/752663292698427422/hannie2.gif',
-            'https://cdn.discordapp.com/attachments/702535108229333004/752663508470071377/hannie3.gif'
-    ];
+                    'https://cdn.discordapp.com/attachments/702535108229333004/752663292698427422/hannie2.gif',
+                    'https://cdn.discordapp.com/attachments/702535108229333004/752663508470071377/hannie3.gif'
+                    ];
+
         let randomgetal = Math.floor(Math.random()*gifs.length);
+
         const gifEmbed = new Discord.MessageEmbed()
         .setColor('#f9c901')
         .setTitle('hi!')
         .setImage(gifs[randomgetal]);
         message.channel.send(gifEmbed);
+
     } else if (command === 'help'){
+
         const helpEmbed = new Discord.MessageEmbed()
         .setColor('#f9c901')
         .setThumbnail('https://cdn.discordapp.com/attachments/702535108229333004/752666587407843418/unknown.png')
         .setTitle('commands this bot can do:')
-        .setDescription('*hello command*\n```&hey``` - answers you\n*stream command*\n```&stream``` - gives you a stray kids title track to stream\n*wave command*\n```&wave``` - waves for you\n*kiss command*\n```&kiss``` - sends kiss\n*minsung command*\n```&minsung``` - gives you an uwu gif\n*game command*\n```&game``` - play a game! parameters: ```[gender]:(male/female)``` ```[diffivulty]:(easy/medium/hard)```');
+        .setDescription('*hello command*\n```&hey``` - answers you\n*stream command*\n```&stream``` - gives you a stray kids title track to stream\n*wave command*\n```&wave``` - waves for you\n*kiss command*\n```&kiss``` - sends kiss\n*minsung command*\n```&minsung``` - gives you an uwu gif\n*game command*\n```&game [gender][difficulty]``` - play a game! parameters: ```[gender]:(male/female)``` ```[difficulty]:(easy/medium/hard)```');
+
         message.channel.send(helpEmbed);
+
     } else if (command === 'kiss'){
+
         const gifs = ['https://cdn.discordapp.com/attachments/702535108229333004/752859240619835414/hannie5.gif',
-            'https://cdn.discordapp.com/attachments/702535108229333004/752859281849712650/hannie6.gif',
-            'https://cdn.discordapp.com/attachments/702535108229333004/752859316414971924/hannie7.gif'
-    ];
+                    'https://cdn.discordapp.com/attachments/702535108229333004/752859281849712650/hannie6.gif',
+                    'https://cdn.discordapp.com/attachments/702535108229333004/752859316414971924/hannie7.gif'
+                    ];
+
         let randomgetal = Math.floor(Math.random()*gifs.length);
+
         const gifEmbed = new Discord.MessageEmbed()
         .setColor('#f9c901')
         .setTitle('chuu <3')
         .setImage(gifs[randomgetal]);
+
         message.channel.send(gifEmbed);
+
     } else if (command === 'minsung'){
+
         const gifs = ['https://cdn.discordapp.com/attachments/702535108229333004/752861899699388507/minsung2.gif',
             'https://cdn.discordapp.com/attachments/702535108229333004/752861899074306138/minsung1.gif',
             'https://cdn.discordapp.com/attachments/702535108229333004/752861968095772782/minsung4.gif',
@@ -77,15 +92,20 @@ client.on('message', message => {
             'https://cdn.discordapp.com/attachments/702535108229333004/752995613410328601/minsung9.gif',
             'https://cdn.discordapp.com/attachments/702535108229333004/752995617801764895/minsung8.gif',
             'https://cdn.discordapp.com/attachments/702535108229333004/754067252873265344/minsung10.gif'
-    ];
+            ];
+
         let randomgetal = Math.floor(Math.random()*gifs.length);
+
         const gifEmbed = new Discord.MessageEmbed()
         .setColor('#f9c901')
         .setTitle('uwu')
         .setImage(gifs[randomgetal]);
         message.channel.send(gifEmbed);
+
     }else if (command === 'game'){
+
         let idols = JSON.parse(fs.readFileSync('idols.json', 'utf-8'));
+
         if(args.length){
         if(args[0] === 'male'){
              
@@ -98,11 +118,13 @@ client.on('message', message => {
              
             
         }else{
-        message.channel.send('incorrect parameters!'); return;}}
+        message.channel.send('incorrect parameters!'); return;}
+        }
         
     
     let randomIdol;
     let randomFoto;
+
     const filter = response => {
         return idols[randomIdol].name.some(naam => naam.toLowerCase() === response.content.toLowerCase());
         
@@ -113,13 +135,13 @@ client.on('message', message => {
         
 
         randomIdol = Math.floor(Math.random()*idols.length);
-     randomFoto = Math.floor(Math.random()*idols[randomIdol].pictures.length);
+        randomFoto = Math.floor(Math.random()*idols[randomIdol].pictures.length);
      
     
-    message.channel.send(idols[randomIdol].pictures[randomFoto])
-    .then(() => {
+        message.channel.send(idols[randomIdol].pictures[randomFoto])
+        .then(() => {
         
-        message.channel.awaitMessages(filter, {max : 1, time: 15000, errors: ['time']}).then(collected => {
+            message.channel.awaitMessages(filter, {max : 1, time: 15000, errors: ['time']}).then(collected => {
             collected.first().react('✅');
             punten++;
             message.channel.send(`correct! the idol was ${idols[randomIdol].group} ${idols[randomIdol].name[0]} \nlives : ${lives}, points : ${punten}`);
@@ -148,21 +170,14 @@ client.on('message', message => {
             else{
                 message.channel.send(`game over!`);
                 return;
-            }
+                }
             
-        })
-        ;
-    }); 
+            });
+        }); 
     
-    
+    };
 
-
-};
-
-spel(3,0);
-
-
-    
+    spel(3,0);
 
 };
 
